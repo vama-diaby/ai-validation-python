12b1235b93b511980faaf4d33f4d34ac1fe99488
kassamba <314480549+vama-diaby@users.noreply.github.com>
2026-09-12 22:39:55 -0400

    Add models list exercise

---
+++ exercises/01_python_basics/lists.py
@@ -0,0 +1,11 @@
+models = [
+    "model-a",
+    "model-b",
+    "model-c"
+]
+
+print("First model:", models[0])
+print("Last model:", models[-1])
+print("Number of models:", len(models))
+
+models.append("model-d")
