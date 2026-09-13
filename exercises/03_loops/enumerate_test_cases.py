3479773d2681a44df439e1356afb822a1d8058dc
kassamba <314480549+vama-diaby@users.noreply.github.com>
2026-09-13 17:43:45 -0400

    Add range and enumerate exercises

---
+++ exercises/03_loops/enumerate_test_cases.py
@@ -0,0 +1,9 @@
+test_cases = [
+    "T01",
+    "T02",
+    "T03",
+    "T04",
+]
+
+for number, test_case in enumerate(test_cases, start=1):
+    print(str(number) + ".", test_case)
---
+++ exercises/03_loops/range_runs.py
@@ -0,0 +1,2 @@
+for run_number in range(1, 6):
+    print("Run", run_number)
